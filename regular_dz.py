4b3c2f5c78645e9d99159cb363c351343ff93c9b
polanko78 <47469700+polanko78@users.noreply.github.com>
2019-04-22 22:39:57 +0300

    1

--- regular_dz.py
+++ regular_dz.py
@@ -1,105 +1,42 @@
-from pprint import pprint
 import re
-# читаем адресную книгу в формате CSV в список contacts_list
 import csv
+
 with open("phonebook_raw.csv", encoding='utf8') as f:
-  rows = csv.reader(f, delimiter=",")
-  contacts_list = list(rows)
-#pprint(contacts_list)
+    rows = csv.reader(f, delimiter=",")
+    contacts_list = list(rows)
 
-# TODO 1: выполните пункты 1-3 ДЗ
-# ваш код
-new_contacts_list = []
-tmp_dic = {'﻿"lastname':[],
-           'firstname':[],
-           'surname':[],
-           'organization':[],
-           'position':[],
-           'phone':[],
-           'email':[]
-           }
+
 for i in contacts_list:
-#  if i[0] == '﻿"lastname':
-#    new_contacts_list.append(i)
-  if i[0] != '﻿"lastname':
-      phone = re.compile(
-        '(\+7|8)?[\s]*[\(]*(\d\d\d)[\)]*[\s-]*(\d\d\d)[\s-]*(\d\d)[\s-]*(\d+)\s*([\(]*([а-я]*.)*\s(\d+)[\)]*)*')
-      res = re.sub(phone, r'+7(\2)\3-\4-\5 \7\8', i[5])
-      i[5] = res
-      name_line = i[0] + i[1] + i[2]
-      name = re.compile('([А-Я][а-я]*)\s*([А-Я][а-я]*)\s*([А-Я][а-я]*)*')
-      name_str = re.sub(name, r'\1 ,\2 ,\3 ', name_line)
-      res = re.split(' ,', name_str)
-      i[0] = res[0]
-      i[1] = res[1]
-      i[2] = res[2]
-new_list = []
-new_line = [12]
+    if i[0] != '﻿lastname':
+        phone = re.compile(
+            '(\+7|8)?[\s]*[\(]*(\d\d\d)[\)]*[\s-]*(\d\d\d)[\s-]*(\d\d)[\s-]*(\d+)\s*([\(]*([а-я]*.)*\s(\d+)[\)]*)*')
+        res = re.sub(phone, r'+7(\2)\3-\4-\5 \7\8', i[5])
+        i[5] = res
+        name_line = i[0] + i[1] + i[2]
+        name = re.compile('([А-Я][а-я]*)\s*([А-Я][а-я]*)\s*([А-Я][а-я]*)*')
+        name_str = re.sub(name, r'\1 ,\2 ,\3 ', name_line)
+        res = re.split(' ,', name_str)
+        i[0] = res[0]
+        i[1] = res[1]
+        i[2] = res[2]
+list_to_file = []
+new_line = ['﻿lastname', 'firstname', 'surname', 'organization', 'position', 'phone', 'email']
 tmp_list = contacts_list
-for i in tmp_list:
+for it in tmp_list:
     for t in contacts_list:
-            if t[0] in i:
-                i.remove(t[0])
-                i[0] = t[0]
+        if t[0] == it[0]:
+            for x in [1, 2, 3, 4, 5, 6]:
+                if (t[x] == '' or ' ') and it[x] != '' and it[x] not in new_line:
+                    t[x] = it[x]
+            try:
+                if t[7] == '':
+                    t.pop()
+            except IndexError:
+                pass
+for l in contacts_list:
+    if l not in list_to_file:
+        list_to_file.append(l)
 
-
-
-
-
-
-        new_list.append(new_line)
-
-#            if t[1] == '':
-#                print('!')
-#                t[1] = i[1]
-#            if t[1] == '':
-#                print('!')
-#                t[2] = i[2]
-#            if t[1] == '':
-#                print('!')
-#                t[3] = i[3]
-#            if t[1] == '':
-#                t[4] = i[4]
-#            if t[1] == '':
-#                t[5] = i[5]
-#            if t[1] == '':
-#                t[6] = i[6]
-#        print(t)
-
-
-
-
-
-
-#      for x in new_contacts_list:
-#        if x[0] == i[0]:
-#          if x[1] == '':
-#              x[1] = i[1]
-#          if x[1] == '':
-#              x[2] = i[2]
-#          if x[1] == '':
-#              x[3] = i[3]
-#          if x[1] == '':
-#              x[4] = i[4]
-#          if x[1] == '':
-#              x[5] = i[5]
-#          if x[1] == '':
-#              x[6] = i[6]
-
-
-pprint(new_list)
-
-
-
-# TODO 2: сохраните получившиеся данные в другой файл
-# код для записи файла в формате CSV
-#with open("phonebook.csv", "w") as f:
-#  datawriter = csv.writer(f, delimiter=',')
-#  # Вместо contacts_list подставьте свой список
-#  datawriter.writerows(contacts_list)
-
-#phone = re.compile('(\+7|8)?[\s]*[\(]*(\d\d\d)[\)]*[\s-]*(\d\d\d)[\s-]*(\d\d)[\s-]*(\d+)\s*([\(]*([а-я]*.)*\s(\d+)[\)]*)*')
-#res = re.sub(phone, r'+7(\2)\3-\4-\5 \7\8', data)
-# (\+7|8)?[\s]*[\(]*(\d\d\d)[\)]*[\s-]*(\d\d\d)[\s-]*(\d\d)[\s-]*(\d+)\s*([\(]*([а-я]*.)*\s(\d+)[\)]*)*
-
-# +7($2)$3-$4-$5 $7$8
+with open("phonebook.csv", "w", encoding='utf8') as f:
+    datawriter = csv.writer(f, delimiter=',')
+    datawriter.writerows(list_to_file)
