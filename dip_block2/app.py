3714e3a411760d95f2a70a6bec6a1a4f12b1276d
Dmitry <polanko78@yandex.ru>
2019-05-14 16:27:50 +0300

    !

--- dip_block2/app.py
+++ dip_block2/app.py
@@ -30,6 +30,7 @@ def get_user_data():
         for i in res['response']:
             user_id = i['id']
             bdate = i['bdate']
+            age = int(datetime.strftime(datetime.now(), "%Y")) - int(bdate[-4:])
             books = i['books']
             interests = i['interests']
             movies = i['movies']
@@ -39,7 +40,7 @@ def get_user_data():
 #            x = False
     except KeyError:
         print('{}'.format(res['error']['error_msg']))
-    return user_id, token, bdate, books, interests, movies, music, relation, sex
+    return user_id, token, age, books, interests, movies, music, relation, sex
 #    request = urllib.request.urlopen(
 #        'http://oauth.vk.com/authorize?client_id=' + app + '&        scope=' + settings + '&redirect_uri=' + redirect_url + '&display=' + display + '&response_type=token')
 #    o = urlparse(request)
@@ -62,24 +63,24 @@ def search(token):
         s = 0
     else:
         s = 1
-    time = datetime.now()
-    date = datetime.date(time)
-    bd = datetime.strptime(user.bdate, '%d.%m.%Y')
-    age =
-
 
     params = {
         'sex': s,
+        'age_from': user.age - 2,
+        'age_to': user.age + 2,
         'count': 1000,
         'access_token': token,
         'fields': 'bdate, books, interests, music, movies, relation, sex',
         'v': 5.92
     }
-    response = requests.get('https://api.vk.com/method/users.get', params)
+    response = requests.get('https://api.vk.com/method/users.search', params)
+    res = response.json()
+    pprint(res)
+
 
 if __name__ == '__main__':
-    user_id, token, bdate, books, interests, movies, music, relation,sex = get_user_data()
-    user = VK_USER(token, user_id, bdate, books, interests, movies, music, relation, sex)
+    user_id, token, age, books, interests, movies, music, relation,sex = get_user_data()
+    user = VK_USER(token, user_id, age, books, interests, movies, music, relation, sex)
     search(token)
 #    response_gr = requests.get('https://api.vk.com/method/groups.get', user.params)
 #    r = response_gr.json()
--- dip_block2/class_vk.py
+++ dip_block2/class_vk.py
@@ -1,9 +1,9 @@
 class VK_USER:
 
-    def __init__(self, token, user_id, bdate, books, interests, movies, music, relation, sex):
+    def __init__(self, token, user_id, age, books, interests, movies, music, relation, sex):
         self.token = token
         self.user_id = user_id
-        self.bdate = bdate
+        self.age = age
         self.books = books
         self.interests = interests
         self.movies = movies
